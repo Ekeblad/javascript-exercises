cf8152446af199d8170a5149e5309f8b5370eb1e
Andreas Ekeblad <aekeblad@gmail.com>
2022-03-25 14:50:45 +0100

    completed the 02 text exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num >= 0) {
+    let returnString = "";
+    for (let i = 0; i < num; i++) {
+      returnString += str;
+    }
+    return returnString;
+  } else {
+    return (returnString = "ERROR");
+  }
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
