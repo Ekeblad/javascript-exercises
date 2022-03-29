f127ddbf263fc3d958e972c906a09404bfb2debf
Andreas Ekeblad <aekeblad@gmail.com>
2022-03-29 14:54:26 +0200

    completed the 06 test exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  //divisible by 4 and 400 but not 100
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
