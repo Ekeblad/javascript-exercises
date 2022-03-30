540c54ce14a48ca003755659ea72a5590e97939c
Andreas Ekeblad <aekeblad@gmail.com>
2022-03-30 07:39:19 +0200

    completed the 07 test exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (farenheightIn) {
+  let celciusOut = ((farenheightIn - 32) * 5) / 9;
+  return (celciusOut = Math.round(celciusOut * 10) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function (celciusIn) {
+  let farenheightOut = celciusIn * (9 / 5) + 32;
+  return (farenheightOut = Math.round(farenheightOut * 10) / 10);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
