86ba91735b6555711b2c671429b6d19c10ace15e
Andreas Ekeblad <aekeblad@gmail.com>
2022-05-24 15:56:58 +0200

    completed the 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  const reverseString = string.split("").reverse().join("");
+  if (reverseString === string) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
