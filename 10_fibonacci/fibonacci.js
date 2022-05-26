c8e4e963b9e1e0edf2c4b4c72f39738cf193e2cb
Andreas Ekeblad <aekeblad@gmail.com>
2022-05-26 21:08:32 +0200

    completed the no 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  if (number === 0) return 0;
+  let first = 0;
+  let second = 1;
+  for (let i = 1; i < number; i++) {
+    const temp = second;
+    second = first + second;
+    first = temp;
+  }
+  return second;
 };
 
 // Do not edit below this line
